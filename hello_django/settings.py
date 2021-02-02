d4cd0de657025599ae02b12b27a73ca372cb69e2
Amanda Simmons <carpe737diem@gmail.com>
2021-02-02 13:22:31 -0500

    followed along with Joe's demo in order to hook up a view

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f#fa1va-jd4xu$-1er*h+n$)#g7@#&tvsqeg@p+9v8d355wrxo'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET',
+    'f#fa1va-jd4xu$-1er*h+n$)#g7@#&tvsqeg@p+9v8d355wrxo'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello_app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'hello_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
